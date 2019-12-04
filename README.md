d538df41bf06382c3feddf9e10b3b23e4e049b93
Roy Lenferink <lenferinkroy@gmail.com>
2019-12-04 20:34:41 +0100

    Added initial version of plugin to download shared libraries from Nexus

---
+++ README.md
@@ -0,0 +1,176 @@
+# Nexus Shared Libraries Retriever plugin
+
+The current official plugin [workflow-cps-global-lib](https://github.com/jenkinsci/workflow-cps-global-lib-plugin/) does provide a way to retrieve 
+shared libraries through a SCM, such as Git. The goal of this plugin is to provide another way to retrieve shared libraries via the @Library 
+declaration in a Jenkinsfile.
+
+## Context
+
+The Nexus shared library retriever is implemented to retrieve [shared libraries](https://jenkins.io/doc/book/pipeline/shared-libraries/) from Nexus.
+
+## Installation of the plugin
+
+### Installing from source
+
+You can follow the steps hereafter to install the plugin:
+
+* Clone the sources of this repository
+* Run `mvn package` in order to compile the sources and package them in a .hpi file
+* Follow the [instructions that can be found on the Jenkins website](https://jenkins.io/doc/book/managing/plugins/#installing-a-plugin) and install the plugin 
+that has been packaged in `target/jenkins-lib-nexus-plugin.hpi`
+
+### Installing from artifact
+
+#### Installing from the Plugins site
+
+The plugin is referenced as an official plugin in the Jenkins update site, so you should be able to use the integrated search in the Plugin section.
+
+#### To install the plugin manually
+
+You can find the archives of all the version of the Plugin on the [Update site](https://plugins.jenkins.io/workflow-cps-global-lib-nexus).
+You can also Download the wished plugin release from the GitHub releases section.
+The, go to the Jenkins Administration Plugins UI > Advanced section and upload the plugin .hpi file.
+
+## How to use the plugin
+
+### Pre-requisite: package the library
+
+In order to be able to use this plugin, you need to package your library (probably during a Continuous Integration build) and publish it to Nexus
+so Maven can be used to download the artifact again. You should end up with a ZIP file with the following structure:
+
+```bash
+> unzip -vl pipeline-libraries.zip
+Archive:  pipeline-libraries.zip
+ Length   Method    Size  Cmpr    Date    Time   CRC-32   Name
+--------  ------  ------- ---- ---------- ----- --------  ----
+     xxx  Defl:N      xxx  37% 03-21-2019 12:14 7d354e1f  resources/a-rsc-needed
+    xxxx  Defl:N     xxxx  68% 03-21-2019 12:14 fefba77f  src/your/package/YourGroovyClass.groovy
+     xxx  Defl:N      xxx  33% 03-21-2019 12:14 5b6808ab  vars/yourvar.groovy
+     xxx  Defl:N      xxx  39% 03-21-2019 12:14 5842cc52  vars/yourvar.txt
+--------          -------  ---                            -------
+  xxxxxx           xxxxxx  x%                            n files
+```
+
+If you want to package it like this with maven, you can use the following.
+In your pom.xml
+
+```xml
+<build>
+   <plugins>
+      <plugin>
+            <artifactId>maven-assembly-plugin</artifactId>
+            <version>2.6</version>
+            <executions>
+               <execution>
+                  <configuration>
+                        <finalName>your-library</finalName>
+                        <descriptors>
+                           <descriptor>assembly.xml</descriptor>
+                        </descriptors>
+                  </configuration>
+                  <id>make-assembly1</id>
+                  <phase>package</phase>
+                  <goals>
+                        <goal>single</goal>
+                  </goals>
+               </execution>
+            </executions>
+      </plugin>
+   </plugins>
+</build>
+```
+
+The assembly.xml file:
+
+```xml
+<assembly xmlns="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.3"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.3 http://maven.apache.org/xsd/assembly-1.1.3.xsd">
+  <id>lib</id>
+
+  <formats>
+    <format>zip</format>
+  </formats>
+
+  <includeBaseDirectory>false</includeBaseDirectory>
+
+  <fileSets>
+    <fileSet>
+      <directory>.</directory>
+      <outputDirectory>/</outputDirectory>
+      <lineEnding>unix</lineEnding>
+      <includes>
+        <include>src/**</include>
+        <include>vars/**</include>
+        <include>resources/templates/*.json</include>
+        <include>resources/*.sh</include>
+        <include>version.txt</include>
+      </includes>
+    </fileSet>
+  </fileSets>
+
+</assembly>
+```
+
+### Constraints
+
+*The plugin only supports ZIP shared libraries for the moment.*
+
+### Setup of the plugin
+
+The plugin is configurable on the Jenkins UI, at different levels:
+
+* On Global System Configuration page (Administration)
+* On Folder Configuration page
+* On Pipeline Configuration page
+
+Jenkins administrators can set [global libraries on the Administration page](https://jenkins.io/doc/book/pipeline/shared-libraries/#global-shared-libraries), 
+while Jenkins users can set libraries either on the [Folder view](https://jenkins.io/doc/book/pipeline/shared-libraries/#folder-level-shared-libraries) or on 
+the Pipeline view.
+
+The generic documentation to reference a new shared library can be found [here](https://jenkins.io/doc/book/pipeline/shared-libraries/#using-libraries).
+
+### In the Jenkins UI
+
+In Administration / Folder / Pipeline configuration views:
+
+* Go to the Global Pipeline Libraries section and add a new library. You should see 3 different retrieval methods: Modern SCM, Nexus and Legacy SCM.
+* Give it a name and a default version
+* Decide if you want to load it implicitly or not
+![Administration UI](doc/NexusRetrievalSettings.png)
+* Provide the artifact details used to retrieve the shared libraries in the following format: `<groupId>:<artifactId>:<version>:<packaging>:<optional classifier>`. 
+In the example the following shared library is used: `com.roylenferink:jenkins-shared-library:${library.jenkins-lib.version}:zip:lib`.
+
+The artifact details are version-dynamic, it follows the Jenkins standard annotation `${library.<library_name>.version}` that is afterwards replaced either by the 
+default version provided by the admin, or the version specified by the user Jenkinsfile in the @Library annotation.
+
+### Directly in the Jenkinsfile
+
+Users can also retrieve shared libraries from their Jenkinsfile, by defining it with this syntax:
+
+```groovy
+library identifier: 'external-shared-libraries@4.1.0', retriever: nexus(
+   artifactDetails: 'com.roylenferink:jenkins-shared-library:${library.external-shared-libraries.version}:zip:lib')
+```
+
+## Contributing
+
+You can contribute to this plugin by retrieving the source and following the [official Jenkins plugin tutorial](https://wiki.jenkins.io/display/JENKINS/Plugin+tutorial) 
+to install, run, test and package it.
+
+## Release process
+
+You're a maintainer of this repository and need to release a fix? Please follow the instructions below:
+
+* Make sure you fulfilled the requirements that can be found [here](https://jenkins.io/doc/developer/publishing/releasing/)
+* Be sure to be connected to no VPN to have SSH connection working
+* Be sure to have no mirror defined in your global settings.xml (if needed pass --global-settings an-empty-settings.xml as an additional parameter)
+* Place yourself on the master branch of [the jenkinsci fork](https://github.com/jenkinsci/workflow-cps-global-lib-nexus-plugin)
+* ```mvn release:prepare``` and let the plugin increase the patch number (or increase yourself the minor or major)
+* ```mvn release:perform```
+* If things go wrong ```mvn release:clean```
+* Update the [Wiki release notes](https://wiki.jenkins.io/display/JENKINS/Nexus+Shared+Libraries+Retriever+plugin)
+
+## License
+
+This project is licensed under the terms of the [Apache 2.0 License](LICENSE).
---
+++ src/main/java/com/roylenferink/jenkins/plugin/nexus_lib/NexusRetriever.java
@@ -0,0 +1,208 @@
+package com.roylenferink.jenkins.plugin.nexus_lib;
+
+import com.google.common.annotations.VisibleForTesting;
+import hudson.AbortException;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.Node;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.model.TopLevelItem;
+import hudson.slaves.WorkspaceList;
+import jenkins.model.Jenkins;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.workflow.libs.LibraryRetriever;
+import org.jenkinsci.plugins.workflow.libs.LibraryRetrieverDescriptor;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * The goal of this plugin is to provide another way to retrieve shared libraries via the @Library declaration
+ * in a Jenkinsfile.
+ * <p>
+ * The current official plugin (workflow-cps-global-lib) does provide only a way to retrieve shared libraries
+ * through a SCM, such as Git, Subversion, etc.
+ */
+@Restricted(NoExternalUse.class)
+public class NexusRetriever extends LibraryRetriever {
+
+    private final String artifactDetails;
+
+    private final Node jenkins;
+
+    /**
+     * Constructor used for JUnits
+     * @param jenkins The representation of the Jenkins server
+     */
+    @VisibleForTesting
+    NexusRetriever(String artifactDetails, Node jenkins) {
+        this.jenkins = jenkins;
+        this.artifactDetails = artifactDetails;
+    }
+
+    /**
+     * Constructor
+     */
+    @DataBoundConstructor
+    public NexusRetriever(@Nonnull String artifactDetails) {
+        this.jenkins = Jenkins.get();
+        this.artifactDetails = artifactDetails;
+    }
+
+    public String getArtifactDetails() {
+        return artifactDetails;
+    }
+
+    /**
+     * Retrieves the shared library code. Prefer this version of the method.
+     * <p>
+     * Checks first if the library is accessible via a HEAD call. Then retrieves the shared library from Nexus.
+     *
+     * @param name     Name of the library (as specified in the Jenkinsfile @Library)
+     * @param version  Version of the library (as specified in the Jenkinsfile @Library)
+     * @param target   Where the code should be retrieved
+     * @param run      Jenkins context
+     * @param listener Only used to get the logger
+     * @throws Exception if the file cannot be downloaded, archive can't be extracted, workspace is not writable
+     */
+    @Override
+    public void retrieve(@Nonnull String name, @Nonnull String version, @Nonnull FilePath target,
+                         @Nonnull Run<?, ?> run, @Nonnull TaskListener listener) throws Exception {
+        retrieve(name, version, true, target, run, listener);
+    }
+
+
+    /**
+     * Checks first if the library is accessible via a HEAD call. Then retrieves the shared library from Nexus.
+     *
+     * @param name      Name of the library (as specified in the Jenkinsfile @Library)
+     * @param version   Version of the library (as specified in the Jenkinsfile @Library)
+     * @param changelog Not used
+     * @param target    Where the code should be retrieved
+     * @param run       Jenkins context
+     * @param listener  Only used to get the logger
+     * @throws Exception if the file cannot be downloaded, archive can't be extracted, workspace is not writable
+     */
+    @Override
+    public void retrieve(@Nonnull String name, @Nonnull String version, @Nonnull boolean changelog,
+                         @Nonnull FilePath target, @Nonnull Run<?, ?> run, @Nonnull TaskListener listener)
+            throws Exception {
+
+        String artifactDetails = convertVersion(getArtifactDetails(), name, version);
+        String libDir = getDownloadFolder(name, run).getRemote();
+        listener.getLogger().println("=> Library directory for build: '" + libDir + "'");
+
+        String mvnCommand = "mvn dependency:copy -Dartifact=" + artifactDetails + " -DoutputDirectory=" + libDir;
+        Process process = Runtime.getRuntime().exec(mvnCommand);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line);
+            output.append("\n");
+        }
+        reader.close();
+
+        int exitVal = process.waitFor();
+        listener.getLogger().println("=> Downloading library from Nexus");
+        listener.getLogger().print(output.toString());
+
+        if (exitVal == 0) {
+            String artifactId = artifactDetails.split(":")[1]; // 0 = groupId, 1 = artifactId, etc...
+            listener.getLogger().println("=> Looking for artifact id: " + artifactId);
+            Supplier<Stream<Path>> filesSupplier = () -> {
+                try {
+                    return Files.find(Paths.get(libDir), 1, ((path, basicFileAttributes) -> {
+                        File file = path.toFile();
+                        return !file.isDirectory() && file.getName().endsWith(".zip") && file.getName().startsWith(artifactId);
+                    }));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            };
+
+            if (filesSupplier.get().count() == 1) {
+                listener.getLogger().println("=> File found");
+
+                final Path[] destinationPath = new Path[1];
+                filesSupplier.get().forEach((p) -> destinationPath[0] = p);
+
+                if (Files.exists(destinationPath[0])) {
+                    FilePath filePath = new FilePath(destinationPath[0].toFile());
+
+                    listener.getLogger().println("=> About to unzip " + filePath.getRemote());
+                    filePath.unzip(target); // Extract the archive
+                    filePath.delete();      // Delete the archive
+                    listener.getLogger().println("=> Retrieved (" + artifactDetails + ")");
+                } else {
+                    throw new IOException("File " + destinationPath[0].toString() + " does not exist");
+                }
+            } else {
+                throw new IOException("Unable to find library " + artifactId);
+            }
+        } else {
+            throw new Exception("Error downloading artifact (" + exitVal + ")");
+        }
+    }
+
+    private String convertVersion(String input, String name, String version) {
+        Pattern p = Pattern.compile("\\$\\{library." + name + ".version\\}");
+        Matcher match = p.matcher(input);
+
+        return match.find() ? match.replaceAll(version) : input;
+    }
+
+    private FilePath getDownloadFolder(String name, Run<?, ?> run) throws IOException {
+        FilePath dir;
+        if (run.getParent() instanceof TopLevelItem) {
+            FilePath baseWorkspace = jenkins.getWorkspaceFor((TopLevelItem) run.getParent());
+            if (baseWorkspace == null) {
+                throw new IOException("Unable to determine workspace for build, " + jenkins.getDisplayName());
+            }
+            dir = baseWorkspace.withSuffix(getFilePathSuffix() + "libs").child(name);
+        } else {
+            throw new AbortException("Cannot check out in non-top-level build");
+        }
+        return dir;
+    }
+
+    // There is WorkspaceList.tempDir but no API to make other variants
+    private static String getFilePathSuffix() {
+        return System.getProperty(WorkspaceList.class.getName(), "@");
+    }
+
+    // ---------- DESCRIPTOR ------------ //
+
+    @Override
+    public LibraryRetrieverDescriptor getDescriptor() {
+        return super.getDescriptor();
+    }
+
+    @Symbol("nexus")
+    @Extension
+    @Restricted(NoExternalUse.class)
+    public static class DescriptorImpl extends LibraryRetrieverDescriptor {
+        @Override
+        public @Nonnull
+        String getDisplayName() {
+            return "Nexus";
+        }
+    }
+
+}
